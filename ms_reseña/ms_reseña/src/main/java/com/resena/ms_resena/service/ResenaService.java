40e35d31bb6dd278727379fc5c7aad429e244801
alvarosalazarxd <al.salazars@duocuc.cl>
2026-05-24 01:55:28 -0400

    Add files via upload

---
+++ ms_reseña/ms_reseña/src/main/java/com/resena/ms_resena/service/ResenaService.java
@@ -0,0 +1,30 @@
+package com.resena.ms_resena.service;
+
+
+import java.util.List;
+
+import com.resena.ms_resena.dto.ResenaRequestDTO;
+import com.resena.ms_resena.dto.ResenaResponseDTO;
+
+public interface ResenaService {
+
+    ResenaResponseDTO crearResena(
+            ResenaRequestDTO requestDTO);
+
+    List<ResenaResponseDTO> obtenerTodas();
+
+    ResenaResponseDTO obtenerPorId(
+            Long id);
+
+    void eliminarResena(
+            Long id);
+
+    List<ResenaResponseDTO> buscarPorProducto(
+            Long productoId);
+
+    List<ResenaResponseDTO> buscarPorCalificacion(
+            Integer calificacion);
+
+    List<ResenaResponseDTO> buscarPorUsuario(
+            String usuario);
+}
