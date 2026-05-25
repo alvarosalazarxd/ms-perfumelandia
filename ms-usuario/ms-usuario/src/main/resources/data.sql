8383bc2d98a5b9ac325bc4e01fc1c9eea5b074b6
fjluke <elusuarioalvxd@gmail.com>
2026-05-24 20:59:24 -0400

    Delete ms-usuario/ms-usuario directory

--- ms-usuario/ms-usuario/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-/*INSERT IGNORE INTO usuarios (nombre, correo, password, rol, fecha_registro)
-VALUES ('Alvaro', 'alvaro@gmail.com', '1234', 'ADMIN', NOW());
-
-INSERT IGNORE INTO usuarios (nombre, correo, password, rol, fecha_registro)
-VALUES ('Matias', 'matias@gmail.com', '1234', 'CLIENTE', NOW());
